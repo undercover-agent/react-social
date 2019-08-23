0d627933425c046842dfb53e2ac1faf61b976787
Vlad6555z <Vlad6555z@gmail.com>
2019-08-23 11:26:34 +0300

    11

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import h from'./Header.module.css'
+import React from "react";
+import style from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={h.header}>
-            <div className="container flex-box">
-                <h1 className="logotype">
-                    <a href="#">
-                        <img src="751337_logo_512x512.png" alt="" />
-                    </a>
-                </h1>
-                <ul className="inform flex-box">
-                    <li>
-                        <button>Sign in</button>
-                    </li>
-                    <li>
-                        <button>Login in</button>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    );
-}
-
+  const cls = [style.container, "flex-box"];
+  return (
+    <header className={style.header}>
+      <div className={cls.join(" ")}>
+        <h1 className="logotype">
+          <a href="#">
+            <img src="751337_logo_512x512.png" alt="Logotype" />
+          </a>
+        </h1>
+        <ul className="inform flex-box">
+          <li>
+            <button>Sign in</button>
+          </li>
+          <li>
+            <button>Login in</button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
