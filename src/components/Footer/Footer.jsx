24f9a98f8ddac7ae5e15042fa2c78fae7626cf2f
Vlad6555z <Vlad6555z@gmail.com>
2019-08-23 12:53:31 +0300

    add base components navbar
    Signed-off-by: Vlad6555z <Vlad6555z@gmail.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Footer.css'
+import React from "react";
+import style from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer flex-box">
-            <div className="container">
-                <p className="copyright">Copyright © 2019 Developed by Vlad Houston</p>
-            </div>
-        </footer>
-    );
-}
-
+  const cls = [style.Footer];
+  return (
+    <footer className={cls.join(" ")}>
+      <div className="container">
+        <p className="copyright">Copyright © 2019 Developed by Vlad Houston</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
