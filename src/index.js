fa426bfe48512fd608d4903012c158d9f26898f2
Vlad6555z <Vlad6555z@gmail.com>
2019-09-11 11:36:29 +0300

    state date index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const dialogsData = [
-  { id: "1", name: "Name 1", message: "hello kitty" },
-  { id: "2", name: "Name 2", message: "hello man" },
-  { id: "3", name: "Name 3", message: "hello world" }
-];
-
-const postData = [
-  { message: "lorem ipsum", likesCount: 2 },
-  { message: "lorem ipsum, ipsum lorem", likesCount: 12 }
-];
+let state = {
+  dialogsData: [
+    { id: "1", name: "Name 1", message: "hello kitty" },
+    { id: "2", name: "Name 2", message: "hello man" },
+    { id: "3", name: "Name 3", message: "hello world" }
+  ],
+  postData: [
+    { message: "lorem ipsum", likesCount: 2 },
+    { message: "lorem ipsum, ipsum lorem", likesCount: 12 }
+  ]
+};
 
 const app = (
   <BrowserRouter>
-    <App dialogsData={dialogsData} postData={postData} />
+    <App dialogsData={state.dialogsData} postData={state.postData} />
   </BrowserRouter>
 );
 
