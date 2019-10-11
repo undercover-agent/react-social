b4300a44a6e9ff9f82091da20621a811b4fcc443
Vlad6555z <Vlad6555z@gmail.com>
2019-10-11 13:04:31 +0300

    redux reducers

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,5 +1,13 @@
 import { createStore } from "redux";
 
+import { dialogsReducer } from "./dialog-reducer";
+import { profileReducer } from "./profile-reducer";
+
+let reducers = combineReducers({
+  profilePage: profileReducer,
+  dialogPage: dialogsReducer
+});
+
 let store = createStore();
 
 export default store;
